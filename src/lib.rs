3395a5b73241a0a9f14a0715952be6fe7f1e56b0
Sosthène Guédon <sosthene@nitrokey.com>
2023-09-14 11:09:51 +0200

    Leave only tests and random generator in the backend

--- src/lib.rs
+++ src/lib.rs
@@ -1,80 +1,27 @@
 #![cfg_attr(not(test), no_std)]
 
-use core::ops::Range;
-
 use embedded_hal::blocking::delay::DelayUs;
-use hex_literal::hex;
-use littlefs2::path::Path;
-use namespacing::{namespace, Namespace, NamespaceValue, ObjectKind};
-use se05x::{
-    se05x::{
-        commands::{CheckObjectExists, ReadEcCurveList, WriteEcKey},
-        policies::{ObjectAccessRule, ObjectPolicyFlags, Policy, PolicySet},
-        EcCurve, ObjectId, P1KeyType, Se05X, Se05XResult,
-    },
-    t1::I2CForT1,
-};
-use trussed::{types::Location, Bytes};
+use se05x::{se05x::Se05X, t1::I2CForT1};
 
 #[macro_use]
 extern crate delog;
 generate_macros!();
 
-mod trussed_auth_impl;
-use trussed_auth::MAX_HW_KEY_LEN;
-use trussed_auth_impl::{AuthContext, HardwareKey};
-
 mod core_api;
 pub mod manage;
-pub mod namespacing;
-
-/// Need overhead for TLV + SW bytes
-const BACKEND_DIR: &str = "se050-bak";
-
-pub const GLOBAL_ATTEST_ID: ObjectId = ObjectId(hex!("F0000012"));
-
-pub enum Se05xLocation {
-    Persistent,
-    Transient,
-}
-
-impl From<Location> for Se05xLocation {
-    fn from(value: Location) -> Self {
-        match value {
-            Location::Volatile => Self::Transient,
-            Location::External | Location::Internal => Self::Persistent,
-        }
-    }
-}
 
 pub struct Se050Backend<Twi, D> {
     se: Se05X<Twi, D>,
     enabled: bool,
     failed_enable: Option<se05x::se05x::Error>,
-    metadata_location: Location,
-    hw_key: HardwareKey,
-    ns: Namespace,
-    configured: bool,
 }
 
 impl<Twi: I2CForT1, D: DelayUs<u32>> Se050Backend<Twi, D> {
-    pub fn new(
-        se: Se05X<Twi, D>,
-        metadata_location: Location,
-        hardware_key: Option<Bytes<{ MAX_HW_KEY_LEN }>>,
-        ns: Namespace,
-    ) -> Self {
+    pub fn new(se: Se05X<Twi, D>) -> Self {
         Se050Backend {
             se,
             enabled: false,
             failed_enable: None,
-            metadata_location,
-            hw_key: match hardware_key {
-                None => HardwareKey::None,
-                Some(k) => HardwareKey::Raw(k),
-            },
-            ns,
-            configured: false,
         }
     }
 
@@ -95,67 +42,7 @@ impl<Twi: I2CForT1, D: DelayUs<u32>> Se050Backend<Twi, D> {
 
         Ok(())
     }
-
-    fn reselect(&mut self) -> Result<(), trussed::Error> {
-        if let Err(e) = self.se.enable() {
-            self.failed_enable = Some(e);
-        } else {
-            self.failed_enable = None;
-            self.enabled = true;
-        }
-        Ok(())
-    }
-
-    fn configure(&mut self) -> Result<(), trussed::Error> {
-        const REQUIRED_CURVES: [EcCurve; 2] = [EcCurve::NistP256, EcCurve::NistP521];
-        self.enable()?;
-        if self.configured {
-            return Ok(());
-        }
-        let buf = &mut [0; 1024];
-        let configured_curves = self
-            .se
-            .run_command(&ReadEcCurveList {}, buf)
-            .map_err(|_err| {
-                debug!("Failed to list curves: {_err:?}");
-                trussed::Error::FunctionFailed
-            })?;
-        for i in REQUIRED_CURVES {
-            if !configured_curves.ids.contains(&i.into()) {
-                self.se.create_and_set_curve(i).map_err(|_err| {
-                    debug!("Failed to create curve: {_err:?}");
-                    trussed::Error::FunctionFailed
-                })?;
-            }
-        }
-        self.configured = true;
-
-        Ok(())
-    }
 }
 
 #[derive(Default, Debug)]
-pub struct Context {
-    auth: AuthContext,
-    ns: Option<NamespaceValue>,
-}
-
-impl Context {
-    fn with_namespace<'a>(&'a mut self, ns: &Namespace, client_id: &Path) -> ContextNs<'a> {
-        let ns_val = self
-            .ns
-            .get_or_insert_with(|| ns.for_client(client_id).unwrap());
-        ContextNs {
-            ns: *ns_val,
-            auth: &mut self.auth,
-        }
-    }
-}
-
-#[derive(Debug)]
-pub struct ContextNs<'a> {
-    auth: &'a mut AuthContext,
-    ns: NamespaceValue,
-}
-
-const ID_RANGE: Range<u32> = 0x000000FF..0x7FFF0000;
+pub struct Context {}
--- src/manage.rs
+++ src/manage.rs
@@ -1,18 +1,9 @@
 use embedded_hal::blocking::delay::DelayUs;
-use hex_literal::hex;
-use littlefs2::{path, path::Path};
-use se05x::{
-    se05x::{
-        commands::{CreateSession, DeleteAll, VerifySessionUserId, WriteUserId},
-        ObjectId,
-    },
-    t1::I2CForT1,
-};
+use se05x::t1::I2CForT1;
 use serde::{Deserialize, Serialize};
 use trussed::{
     serde_extensions::{Extension, ExtensionClient, ExtensionImpl, ExtensionResult},
     service::ServiceResources,
-    store::Store,
     types::{Bytes, CoreContext},
     Error,
 };
@@ -24,36 +15,13 @@ use crate::Se050Backend;
 #[derive(Debug, Default)]
 pub struct ManageExtension;
 
-/// Factory reset the entire device
-///
-/// This will reset all filesystems as well as the SE050 secure element.
-#[derive(Debug, Deserialize, Serialize, Copy, Clone)]
-pub struct FactoryResetRequest;
-
 /// Test the  SE050
 #[derive(Debug, Deserialize, Serialize, Copy, Clone)]
 pub struct TestSe050Request;
 
 #[derive(Debug, Deserialize, Serialize)]
 pub enum ManageRequest {
-    FactoryReset(FactoryResetRequest),
     TestSe050(TestSe050Request),
-}
-
-impl TryFrom<ManageRequest> for FactoryResetRequest {
-    type Error = Error;
-    fn try_from(request: ManageRequest) -> Result<Self, Self::Error> {
-        match request {
-            ManageRequest::FactoryReset(request) => Ok(request),
-            _ => Err(Error::InternalError),
-        }
-    }
-}
-
-impl From<FactoryResetRequest> for ManageRequest {
-    fn from(request: FactoryResetRequest) -> Self {
-        Self::FactoryReset(request)
-    }
 }
 
 impl TryFrom<ManageRequest> for TestSe050Request {
@@ -61,7 +29,7 @@ impl TryFrom<ManageRequest> for TestSe050Request {
     fn try_from(request: ManageRequest) -> Result<Self, Self::Error> {
         match request {
             ManageRequest::TestSe050(request) => Ok(request),
-            _ => Err(Error::InternalError),
+            // _ => Err(Error::InternalError),
         }
     }
 }
@@ -72,34 +40,15 @@ impl From<TestSe050Request> for ManageRequest {
     }
 }
 
-#[derive(Debug, Deserialize, Serialize, Copy, Clone)]
-pub struct FactoryResetReply;
-
 #[derive(Debug, Deserialize, Serialize, Clone)]
 pub struct TestSe050Reply {
     pub reply: Bytes<1024>,
 }
 
 #[derive(Debug, Deserialize, Serialize)]
+#[allow(clippy::large_enum_variant)]
 pub enum ManageReply {
-    FactoryReset(FactoryResetReply),
     TestSe050(TestSe050Reply),
-}
-
-impl TryFrom<ManageReply> for FactoryResetReply {
-    type Error = Error;
-    fn try_from(request: ManageReply) -> Result<Self, Self::Error> {
-        match request {
-            ManageReply::FactoryReset(request) => Ok(request),
-            _ => Err(Error::InternalError),
-        }
-    }
-}
-
-impl From<FactoryResetReply> for ManageReply {
-    fn from(request: FactoryResetReply) -> Self {
-        Self::FactoryReset(request)
-    }
 }
 
 impl TryFrom<ManageReply> for TestSe050Reply {
@@ -107,7 +56,7 @@ impl TryFrom<ManageReply> for TestSe050Reply {
     fn try_from(request: ManageReply) -> Result<Self, Self::Error> {
         match request {
             ManageReply::TestSe050(request) => Ok(request),
-            _ => Err(Error::InternalError),
+            // _ => Err(Error::InternalError),
         }
     }
 }
@@ -129,81 +78,13 @@ impl<Twi: I2CForT1, D: DelayUs<u32>> ExtensionImpl<ManageExtension> for Se050Bac
         _core_ctx: &mut CoreContext,
         _backend_ctx: &mut Self::Context,
         request: &<ManageExtension as Extension>::Request,
-        resources: &mut ServiceResources<P>,
+        _resources: &mut ServiceResources<P>,
     ) -> Result<<ManageExtension as Extension>::Reply, Error> {
-        self.configure().map_err(|err| {
+        self.enable().map_err(|err| {
             debug_now!("Failed to enable for management: {err:?}");
             err
         })?;
         match request {
-            ManageRequest::FactoryReset(_) => {
-                let mut buf = [b'a'; 128];
-                let data = &hex!("31323334");
-
-                self.se
-                    .run_command(
-                        &WriteUserId {
-                            policy: None,
-                            max_attempts: None,
-                            object_id: ObjectId::FACTORY_RESET,
-                            data,
-                        },
-                        &mut buf,
-                    )
-                    .map_err(|_err| {
-                        debug_now!("Failed to write factory reset user id: {_err:?}");
-                        Error::FunctionFailed
-                    })?;
-                let session = self
-                    .se
-                    .run_command(
-                        &CreateSession {
-                            object_id: ObjectId::FACTORY_RESET,
-                        },
-                        &mut buf,
-                    )
-                    .map_err(|_err| {
-                        debug_now!("Failed to create reset session: {_err:?}");
-                        Error::FunctionFailed
-                    })?;
-
-                self.se
-                    .run_session_command(
-                        session.session_id,
-                        &VerifySessionUserId { user_id: data },
-                        &mut buf,
-                    )
-                    .map_err(|_err| {
-                        debug_now!("Failed to verify reset session: {_err:?}");
-                        Error::FunctionFailed
-                    })?;
-
-                self.se
-                    .run_session_command(session.session_id, &DeleteAll {}, &mut buf)
-                    .map_err(|_err| {
-                        debug_now!("Failed to factory reset: {_err:?}");
-                        Error::FunctionFailed
-                    })?;
-
-                let platform = resources.platform();
-                let store = platform.store();
-                let ifs = &*store.ifs();
-                let efs = &*store.efs();
-                let vfs = &*store.vfs();
-                ifs.remove_dir_all(path!("/")).map_err(|_err| {
-                    debug_now!("Failed to delete ifs: {_err:?}");
-                    Error::FunctionFailed
-                })?;
-                efs.remove_dir_all(path!("/")).map_err(|_err| {
-                    debug_now!("Failed to delete efs: {_err:?}");
-                    Error::FunctionFailed
-                })?;
-                vfs.remove_dir_all(path!("/")).map_err(|_err| {
-                    debug_now!("Failed to delete vfs: {_err:?}");
-                    Error::FunctionFailed
-                })?;
-                Ok(FactoryResetReply.into())
-            }
             ManageRequest::TestSe050(TestSe050Request) => {
                 let mut reply = Bytes::new();
                 se050_tests::run_tests(&mut self.se, &mut reply)?;
@@ -216,13 +97,6 @@ impl<Twi: I2CForT1, D: DelayUs<u32>> ExtensionImpl<ManageExtension> for Se050Bac
 type ManageResult<'a, R, C> = ExtensionResult<'a, ManageExtension, R, C>;
 
 pub trait ManageClient: ExtensionClient<ManageExtension> {
-    /// Factory reset the entire device
-    ///
-    /// This will reset all filesystems as well as the SE050 secure element.
-    fn factory_reset(&mut self) -> ManageResult<'_, FactoryResetReply, Self> {
-        self.extension(FactoryResetRequest)
-    }
-
     /// Test the se050 device and driver
     ///
     /// This will factory reset the SE050, and test that most commands work as expected,
